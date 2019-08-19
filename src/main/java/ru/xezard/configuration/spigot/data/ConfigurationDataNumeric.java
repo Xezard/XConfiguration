a5cbea9ca4e3bc533eea29749b4d3196a8b5f177
Xezard <RRFpXe#o|fSRWkccGAWT>
2019-08-20 01:45:40 +0400

    Removed.

--- src/main/java/ru/xezard/configuration/spigot/data/ConfigurationDataNumeric.java
+++
@@ -1,8 +0,0 @@
-package ru.xezard.configuration.spigot.data;
-
-public abstract class ConfigurationDataNumeric<T extends Number>
-extends ConfigurationData<T>
-{
-    @Override
-    public abstract T getDefault();
-}
